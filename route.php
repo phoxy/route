35d1def102d73089a2db3a36a300ed5375ee2c35
Kirill Berezin <enelar@exsul.net>
2018-02-21 19:35:18 +0200

    Support for rewrite, found, die, echo, cat, not_found, http_code, static directives

--- route.php
+++ route.php
@@ -14,9 +14,10 @@ namespace phoxy\route;
 
 function route($obj)
 {
-  $route = parse($_SERVER['REQUEST_URI']);
+  $request = parse($_SERVER['REQUEST_URI']);
+  $route = $request->url;
 
-  header_log('initial data', $route->url);
+  header_log('initial data', $route);
   header_log('executing against', count($obj->rules), 'rules');
 
   foreach ($obj->rules as $k => $rule)
@@ -29,12 +30,95 @@ function route($obj)
       continue;
     }
 
-    $res = preg_match("/$regexp/", $request, $matches);
+    if ($regexp[0] != '/')
+      $regexp = "/$regexp/";
+    $res = preg_match("$regexp", $route, $matches);
+
+    if ($res === false)
+    {
+      internal\header_rule_log($k, "regexp syntax error", $regexp);
+      header_log('inconsistent configuration');
+      echo "Issue in phoxy/route rule $k";
+      die();
+    }
+
     if (!$res)
     {
-      internal\header_rule_log($k, "doesn't fit");
+      internal\header_rule_log($k, "doesn't fit", $regexp);
       continue;
     }
+
+    internal\header_rule_log($k, "matched", $regexp);
+
+    if ($rule->static
+      && ($rule->static !== true
+        || $rule->static = "{$_SERVER['DOCUMENT_ROOT']}{$route}"))
+      if (!file_exists($rule->static))
+        $rule->not_found = true;
+      else
+      {
+        $rule->die = true;
+
+        $mtime = gmdate('D, d M Y H:i:s', filemtime($rule->static));
+
+        if (@getallheaders()['If-Modified-Since'] == $mtime)
+          $rule->http_code = "304 Not Modified";
+        else
+        {
+          $mime = isset($rule->mime) ? $rule->mime : '';
+          $max_age = isset($rule->max_age) ? $rule->max_age : 600;
+
+          @header("Last-Modified: {$mtime}");
+          @header("Cache-Control: public, max-age={$max_age}");
+          @header("Content-Type: {$mime}");
+
+          @header('Content-Length: ' . filesize($rule->static));
+          readfile($rule->static);
+        }
+      }
+
+    if ($rule->rewrite)
+    {
+      $route = preg_replace("/^.*?$regexp.*?$/", $rule->rewrite, $route);
+      internal\header_rule_log($k, "rewrite to", $route);
+    }
+
+    if ($rule->found)
+    {
+      $route = preg_replace("$regexp", $rule->found, $route);
+      internal\header_rule_log($k, "found at", $route);
+
+      header("HTTP/1.1 307 Found there, use your cache if you could");
+      header("Location: $route");
+      $rule->die = true;
+    }
+
+
+
+    if ($rule->forbid)
+    {
+      $rule->http_code = $rule->echo = "403 Forbidden";
+      $rule->die = true;
+    }
+
+    if ($rule->not_found)
+    {
+      $rule->http_code = $rule->echo = "404 Not Found";
+      $rule->die = true;
+    }
+
+    if ($rule->http_code)
+      header("HTTP/1.1 {$rule->http_code}");
+
+
+
+    if ($rule->cat)
+      echo file_get_contents($rule->cat);
+    if ($rule->echo)
+      echo $rule->echo;
+
+    if ($rule->die)
+      die();
   }
 
   header_log('no suitable action for url');
